44cb6da63be842cd5dfbb4498c66328f37760e96
Bluemooses <60531407+Bluemooses@users.noreply.github.com>
2020-06-21 12:14:14 -0500

    Go home action complete

--- src/components/MovieDetails/MovieDetails.js
+++ src/components/MovieDetails/MovieDetails.js
@@ -1,17 +1,36 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import mapReduxStateToProps from "../Modules/MapReduxState";
+
 class MovieDetails extends Component {
   componentDidMount() {
     console.log("Hello world");
   }
 
+  backButton = () => {
+      this.props.history.push('/');
+  }
+
   render() {
     return (
       <div>
-        <p>
-          <p>{this.props.reduxState.details.description}</p>
-        </p>
+        <section className="title">
+          {this.props.reduxState.details.title}
+        </section>
+        <section>
+          <ul>
+            {this.props.reduxState.genres.map((genre) => {
+              return <li key={genre.name}>{genre.name}</li>;
+            })}
+          </ul>
+        </section>
+        <section className="description">
+          {this.props.reduxState.details.description}
+        </section>
+        <section>
+          <button onClick={this.backButton}>Go Home</button>
+          <button onClick={this.hitEditPage}>Edit</button>
+        </section>
       </div>
     );
   }
